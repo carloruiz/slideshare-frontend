274c5806f666225a77b391c4543f3027a025cebf
Carlo Ruiz <carloruiz.57@gmail.com>
2019-03-04 00:21:31 -0500

    more trouble4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from 'components/Home.jsx';
-import Header from 'components/Header.jsx'
-import SignUp from 'components/Signup.jsx';
-import Upload from 'components/Upload.jsx';
-import Profile from 'components/Profile.jsx';
-import Settings from 'components/Settings.jsx';
-import Login from 'components/Login.jsx';
+import Home from './components/Home.jsx';
+import Header from './components/Header.jsx'
+import SignUp from './components/Signup.jsx';
+import Upload from './components/Upload.jsx';
+import Profile from './components/Profile.jsx';
+import Settings from './components/Settings.jsx';
+import Login from './components/Login.jsx';
 
 const Layout = (Child) => {
   const Wrapper = (props) => {
